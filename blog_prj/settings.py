4f0dd3bd37476010de5ee1eadc3fb973eb6dc6b7
IgorZn <igor.znamensky@gmail.com>
2023-09-03 13:14:51 +0300

    next: Django REST Swagger

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -55,6 +55,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
